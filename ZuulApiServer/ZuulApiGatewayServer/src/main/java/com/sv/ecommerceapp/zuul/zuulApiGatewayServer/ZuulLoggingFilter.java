92d49b5321a750d288bd852917023d1dd103325a
Adarsh Dubey <55585713+asd-lab@users.noreply.github.com>
2020-10-07 18:33:01 +0530

    Add files via upload

---
+++ ZuulApiServer/ZuulApiGatewayServer/src/main/java/com/sv/ecommerceapp/zuul/zuulApiGatewayServer/ZuulLoggingFilter.java
@@ -0,0 +1,44 @@
+package com.sv.ecommerceapp.zuul.zuulApiGatewayServer;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+
+@Component
+public class ZuulLoggingFilter extends ZuulFilter{
+
+	private Logger logger=LoggerFactory.getLogger(this.getClass());  
+	
+	@Override
+	public boolean shouldFilter() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public Object run() throws ZuulException {
+		HttpServletRequest request=RequestContext.getCurrentContext().getRequest();  
+		//prints the detail of the requesting the log  
+		logger.info("request -> {} request uri-> {}", request, request.getRequestURI());
+		return null;
+	}
+
+	@Override
+	public String filterType() {
+		// TODO Auto-generated method stub
+		return "pre";
+	}
+
+	@Override
+	public int filterOrder() {
+		// TODO Auto-generated method stub
+		return 1;
+	}
+
+}
