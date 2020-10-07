c75a5fbb5ea6fbe87dd5b65b5ad32adc7c56fe62
Adarsh Dubey <55585713+asd-lab@users.noreply.github.com>
2020-10-07 18:34:13 +0530

    Add files via upload

---
+++ ZuulApiGatewayServer/src/test/java/com/sv/ecommerceapp/zuul/zuulApiGatewayServer/ZuulApiGatewayServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sv.ecommerceapp.zuul.zuulApiGatewayServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulApiGatewayServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
